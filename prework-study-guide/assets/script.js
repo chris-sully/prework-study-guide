2b4b11d4e8a4d9c2c3c4fa6b9d871477f755c247
chris sully <chris.sullivan31@outlook.com>
2023-06-21 21:22:00 -0400

    topic functiions

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,10 +1,12 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(var x = 0; x < topics.length; x++) {
+function listTopics() {
+for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
+  }
 }
 
-/*
+function selectTopic() {
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -15,5 +17,6 @@ if (topic === 'HTML') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
+  }
 }
-*/
+
